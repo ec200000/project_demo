188bb8a55a1b2db22b4b7aabfb937acd7331f107
talskopas <skopas@post.bgu.ac.il>
2021-12-23 13:24:59 +0200

    cart screen doesn't work

--- test/cart_screen_test.dart
+++ test/cart_screen_test.dart
@@ -7,12 +7,24 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:project_demo/providers/cart.dart';
+import 'package:provider/provider.dart';
 import '../lib/screens/cart_screen.dart';
 
 void main() {
   testWidgets('Cart Screen test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new Directionality(textDirection: TextDirection.ltr, child: MediaQuery(data: MediaQueryData(), child: CartScreen())));
+    await tester.pumpWidget(MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        )
+      ],
+      child: MaterialApp(
+          home: new Directionality(textDirection: TextDirection.ltr, child: MediaQuery(data: MediaQueryData(), child: CartScreen()))
+        )
+      )
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('Total'), findsOneWidget);
