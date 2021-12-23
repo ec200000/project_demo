9021855d621621ea47533c6d2002ca4df9ef1ca1
ec200000 <ec200000@gmail.com>
2021-12-23 10:37:32 +0200

    starting adding tests

--- test/auth_screen_test.dart
+++ test/auth_screen_test.dart
@@ -7,13 +7,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:project_demo/main.dart';
+import 'package:project_demo/screens/auth_screen.dart';
+
+import '../lib/screens/auth_screen.dart';
 
 void main() {
-  testWidgets(' test', (WidgetTester tester) async {
+  testWidgets('Auth Screen test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(AuthScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -28,20 +30,4 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
 }
