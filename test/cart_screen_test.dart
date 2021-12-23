319ba1fd3bb798039b43f9fc6249076d0327f041
talskopas <skopas@post.bgu.ac.il>
2021-12-23 11:10:24 +0200

    cart screen test

---
+++ test/cart_screen_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/screens/cart_screen.dart';
+
+void main() {
+  testWidgets('Cart Screen test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new Directionality(textDirection: TextDirection.ltr, child: MediaQuery(data: MediaQueryData(), child: CartScreen())));
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Total'), findsOneWidget);
+    expect(find.text('ORDER NOW'), findsOneWidget);
+  });
+}
