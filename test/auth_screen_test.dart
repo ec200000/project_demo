dfc86c26ea2d30f93246bbdfc516aa66e299aa7f
ec200000 <ec200000@gmail.com>
2021-12-23 10:57:02 +0200

    starting adding tests

--- test/auth_screen_test.dart
+++ test/auth_screen_test.dart
@@ -7,27 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project_demo/main.dart';
-import 'package:project_demo/screens/auth_screen.dart';
-
 import '../lib/screens/auth_screen.dart';
 
 void main() {
   testWidgets('Auth Screen test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(AuthScreen());
+    await tester.pumpWidget(new Directionality(textDirection: TextDirection.ltr, child: MediaQuery(data: MediaQueryData(), child: AuthScreen())));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Sign in with Google'), findsOneWidget);
+    expect(find.text('Sign in with Facebook'), findsOneWidget);
+    expect(find.text('Sign in with Amazon'), findsNothing);
   });
-
 }
